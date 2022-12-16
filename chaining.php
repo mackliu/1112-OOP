cf704339852cbe702ce7acdc453bd91b9e756b53
mackliu <mackliu@wda.gov.tw>
2022-12-16 14:16:25 +0800

    方法鏈的介紹與使用

---
+++ chaining.php
@@ -0,0 +1,37 @@
+<?php
+
+$car=new Car("yellow");
+echo $car->addColor('blue')
+         ->addColor('sky')
+         ->addColor('red')
+         ->addColor('棕色')
+         ->getColor();
+echo "<br>";
+//echo $car->addColor('blue');
+
+class Car{
+
+    protected $color;
+
+    public function __construct($color)
+    {
+        $this->color=$color;
+    }
+
+    function getColor(){
+        return $this->color;
+    }
+
+    function addColor($color){
+
+        $this->color=$this->color."+".$color;
+
+        return $this;
+    }
+
+    
+
+}
+
+
+?>
